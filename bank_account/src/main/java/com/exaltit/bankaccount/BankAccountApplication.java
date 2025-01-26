16379834acbeeafc102a96b22200a26f9ab732e2
bramli ahmed khalil <ahmed.khalil.bramli@gmail.com>
2025-01-26 21:51:02 +0100

    add springdoc-openapi

--- bank_account/src/main/java/com/exaltit/bankaccount/BankAccountApplication.java
+++ bank_account/src/main/java/com/exaltit/bankaccount/BankAccountApplication.java
@@ -1,8 +1,17 @@
 package com.exaltit.bankaccount;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Bank Account Hexagonal Architecture",
+        version = "1.0",
+        description = "Bank Account API for managing resources"
+    )
+)
 @SpringBootApplication
 public class BankAccountApplication {
 
